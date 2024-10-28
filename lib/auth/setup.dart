276407a7c5964b9a9f3dba432a43ac5bd9e157b8
french-femboi <cat@catpawz.net>
2024-10-28 08:51:34 +0100

    Refactor setup page title to 'LWC Setup'

--- lib/auth/setup.dart
+++ lib/auth/setup.dart
@@ -1,24 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
 import '../inc/haptic.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'LWC Setup',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: SetupPage(),
-    );
-  }
-}
 
 class SetupPage extends StatefulWidget {
   @override
